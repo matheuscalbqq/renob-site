f4c7a74f6764c01ce55898edbc8a53ec866a767d
Matheus Cavalcanti <matheusc.albqq@gmail.com>
2025-06-25 16:47:22 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#597eec',    // Masculino
+        secondary: '#f76482',  // Feminino
+        tertiary: '#57bb7f',   // Todos
+        tooltip: '#ffffff',
+        tooltipBorder: '#dddddd',
+      },
+      boxShadow: {
+        tooltip: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+      },
+      fontSize: {
+        tooltip: '14px',
+      }
+    },
+  },
+  plugins: [],
+}
+
