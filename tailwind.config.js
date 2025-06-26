7b76074831f0bccb71af3741e2b3466d62448123
Matheus Cavalcanti <matheusc.albqq@gmail.com>
2025-06-26 10:25:24 -0300

    atualização tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html"],
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js"
+  ],
   theme: {
     extend: {
       colors: {
